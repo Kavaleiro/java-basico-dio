803da08701e54353ed167251c134e0ae35f5cf7a
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-01-11 01:17:27 +0000

    Linha Primeira

---
+++ primeiro-projeto-java-git/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+        
+        System.out.print( "Minha primeira linha de codigo escrita TOTALMENTE por mim." );
+    }
+
+}
