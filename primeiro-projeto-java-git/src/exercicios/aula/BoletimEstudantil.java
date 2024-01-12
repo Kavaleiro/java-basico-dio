709778bc26cdeace998fed4c0f607876b7b9886c
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-01-12 02:20:59 +0000

    bora

---
+++ primeiro-projeto-java-git/src/exercicios/aula/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package exercicios.aula;
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 4;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        if (mediaFinal == 6)
+            System.out.println("PROVA DE MINERVA");
+        if (mediaFinal > 6);
+            System.out.println("APROVADO");
+    }
+}
