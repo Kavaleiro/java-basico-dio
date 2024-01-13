2aed38caba57b3911668984d857b1d28ad367645
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-01-13 18:50:10 +0000

    ExercicioMetodos

--- primeiro-projeto-java-git/src/exercicios/aula/exercicioss/Usuario.java
+++ primeiro-projeto-java-git/src/exercicios/aula/exercicioss/Usuario.java
@@ -6,12 +6,18 @@ public class Usuario {
 
         smartv.diminuirVolume();
         smartv.diminuirVolume();
+        System.out.println("Volume atual: " +smartv.volume);
+
+        System.out.println("Voce esta no Canal: " +smartv.canal);
+        System.out.print("Mudando de Canal...");
+        smartv.mudarCanal(13);
+        System.out.println("Seu Canal atual é: " +smartv.canal);
 
         System.out.println("TV Ligada  " +smartv.ligada);
         System.out.println("Canal Atual " +smartv.canal);
         System.out.println("Volume Atual " +smartv.volume);
     
-        smartv.ligar();
+        smartv.desligar();
         System.out.println("Novo status ---> Tv ligada " +smartv.ligada);
     }
     
