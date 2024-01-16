d24cf1379ae449b0b03b1fb3b14127f97f30d910
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-01-16 03:43:55 +0000

    Aulas de terminal

---
+++ primeiro-projeto-java-git/src/exercicios/aula/AboutMe.java
@@ -0,0 +1,30 @@
+package exercicios.aula;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite a sua idade ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+        
+
+
+        //imprimindo os dados obtidos!!
+        System.out.println("Oi, me chamo " + nome + " "+sobrenome);
+        System.out.println("Tenho "+idade+" anos" );
+        System.out.println("Minha Altura e "+altura+ " cm");
+    }
+    
+}
---
+++ primeiro-projeto-java-git/src/exercicios/aula/Calculadora.java
@@ -0,0 +1,23 @@
+package exercicios.aula;
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operacoes matematicas entre numero inteiros
+ * 
+ * Leia atentamente a documentacao desta classe
+ * para desfrutar dos recurso oferecidos pelo autor
+ * 
+ * @author Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022
+ */
+public class Calculadora {
+    /**
+     * Este metodo e utilizado para somara dois numeros inteiros
+     * @param numeroUm este e o primeiro parametro do metodo
+     * @param numeroDois este e o segundo paramentro do metodo
+     * @return int o resultado deste metodo e a soma de dois numeros.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
