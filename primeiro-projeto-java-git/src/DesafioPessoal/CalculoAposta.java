20dd264f12c167f4d20e5b1cf3e1430128f3be7e
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-01-29 03:02:24 +0000

    DesafioPessoalFinal

--- primeiro-projeto-java-git/src/DesafioPessoal/CalculoAposta.java
+++ primeiro-projeto-java-git/src/DesafioPessoal/CalculoAposta.java
@@ -2,23 +2,21 @@ package DesafioPessoal;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CalculoAposta {
 
     public static void main(String[] args) {
         
-        int i = 1; int x = 1;
         int sair;
+        double totganho = 0;
+        double totperda = 0;
 
+        ArrayList <Double> ganho = new ArrayList<>();
+        ArrayList <Double> perda = new ArrayList<>();
 
         try {
             Scanner sc = new Scanner(System.in);
             do {
-
-                ArrayList <Double> ganho = new ArrayList<>();
-                ArrayList <Double> perda = new ArrayList<>();
-
                 System.out.println("Qual foi o seu ganho:");
                 while( sc.hasNextDouble()){
                     ganho.add(sc.nextDouble());
@@ -30,20 +28,25 @@ public class CalculoAposta {
                 System.out.println("[1]-----CONTINUAR");
                 System.out.println("[0]-----SAIR");
                 sair = sc.nextInt();
-                if(sair == 0){
-                    System.out.println("Muito obrigado por participar!!");
-                    System.out.print("\n-----SEUS GANHOS-----");
-                    for( i = 1; i < ganho.size(); i++) {
-                        System.out.println("["+i+"]-----"+ganho);
-                    }
-
-                    System.out.print("\n-----SUAS PERDAS-----");
-                    for(double p : perda){
-                        System.out.println("["+x+"]-----"+perda);
-                        x++;
-                    }
-                }
             }while(sair!=0);
+                System.out.println("Muito obrigado por participar!!");
+                System.out.print("\n-----SEUS GANHOS-----\n");
+                
+            for( int i = 0; i < ganho.size(); i++) {
+                System.out.println("["+(i+1)+"]-----  "+ganho.get(i));
+                totganho +=  ganho.get(i);
+
+            }
+
+            System.out.print("\n-----SUAS PERDAS-----\n");
+            for(int x = 0; x < perda.size(); x++){
+                System.out.println("["+(x+1)+"]-----  "+perda.get(x));
+                totperda += perda.get(x);
+            }
+            Double saldo = totganho - totperda;  
+            System.err.println("=====================================");
+            System.err.println("SEU SALDO----------"+ saldo);     
+            System.err.println("=====================================");     
 
         }catch(Exception e){
             System.out.println("Voce digitou um valor inválido!!");
