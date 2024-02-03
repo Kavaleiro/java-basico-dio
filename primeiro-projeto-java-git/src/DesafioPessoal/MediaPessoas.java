31506911b0cc4108429412dccf32885a5fd095be
Felipe Barboza <109117017+Kavaleiro@users.noreply.github.com>
2024-02-03 16:40:08 +0000

    Desafio pessoal 2

--- primeiro-projeto-java-git/src/DesafioPessoal/MediaPessoas.java
+++ primeiro-projeto-java-git/src/DesafioPessoal/MediaPessoas.java
@@ -1,56 +1,45 @@
 package DesafioPessoal;
+/*O Desafio é fazer um algoritmo que calcule e esreva:
+ * [A]--- A Maior e a Menor altura.
+ * [B]--- A média de altura dos homens
+ * [C]--- Número de Mulheres
+ */
 
 import java.util.Scanner;
 
-
-import java.util.ArrayList;
-
 public class MediaPessoas {
-    public static void main(String[] args) {
+    public static void main(String [] args){
 
-        String sexo;
-        int numeroMulher = 0;
-        Double maiorAltura = 0.0;Double menoraltura = 20000.0;
+        Double maiorAltura = 0.0; Double menorAltura = 0.0; Double mediaAlturaHomem = 0.0;
+        int numeroMulher = 0; int genero = 0;
+        boolean verifica = false;
+        
+        Scanner input = new Scanner(System.in);
 
-        ArrayList <String> nome = new ArrayList<>();
-        ArrayList <Double> altHomem = new ArrayList<>();
+        for(int x = 0; x < 3; x++ ) {
+            System.out.println("Qual o seu genero: ");
+            System.out.println("[1]-----MULHER");
+            System.out.println("[2]-----HOMEM");
 
-        Scanner sc = new Scanner(System.in);
-        try {
-        for(int x = 0; x < 3; x++){
+            do {
+                genero = input.nextInt();
+                if (genero == 1 || genero == 2){
+                    verifica = true;
+                    break;
+                } else {
+                    System.out.println("ESSA É UMA OPÇÃO INVÁLIDA.");
+                    System.out.println("Por favor escolha umas das opções.");
+                    System.out.println("Qual o seu genero: ");
+                    System.out.println("[1]-----MULHER");
+                    System.out.println("[2]-----HOMEM");
+                }
+            } while (verifica !=false);
 
-            System.out.println("Qual o seu nome: ");
-            nome.add(sc.next());
-            System.out.println("Qual o seu sexo: [M/F]");
-            sexo = sc.next();
-            if (sexo == "M" || sexo == "m"){
-                numeroMulher++;
-            }
-            System.out.println("Qual sua altura: ");
-            Double altura = sc.nextDouble();
-            if(altura>maiorAltura){
-                maiorAltura = altura;
-            }
-            if(altura< menoraltura){
-                menoraltura = altura;
-            }
-        } 
-        sc.close();
-        }catch(Aplication e){
-            System.out.println(e.getMessage());
+            System.out.println("Qual a sua altura: ");
+            do { 
+                Double altura = input.nextDouble();
+            } while ( verifica !=false);
         }
-
-        System.out.println("============================================");
-        System.out.println("A MAIOR ALTURA FOI ------ "+maiorAltura);
-        System.out.println("A MENOR ALTURA FOI ------ "+menoraltura);
-        System.out.println("A MÉDIA DE ALTURA DOS HOMENS FOI -------- "+altHomem);
-        System.out.println("TOTAL DE MULHERES ---------------- "+numeroMulher );
-        System.out.println("============================================");
-
-        for(int i = 0; i < nome.size();i++){
-            System.out.print(nome.get(i)+ " , ");
-        }
-
-    } 
+    }
     
 }
